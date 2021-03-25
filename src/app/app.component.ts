5d364219be788281bf5a08d7197616c9d59c78b8
ditta2020 <73634175+ditta2020@users.noreply.github.com>
2021-03-25 17:54:15 +0100

    partial solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,22 +12,29 @@ export class AppComponent {
 
   todos$: Observable<Todo[]> = this.todoService.getAll();
 
+  phraseString: string = '';
+
   selectedTodo: Todo = new Todo();
 
   constructor(
     private todoService: TodoService,
   ) {}
 
-  onUpdate(product: Product): void {
+ /*  onUpdate(product: Product): void {
     product.active = !product.active;
     this.productService.update(product).subscribe(
     () => console.log("product updated")
   )
-  }
+  } */
 
   onDelete(todo: Todo): void {
     this.todoService.delete(todo).subscribe(
-      console.log("todo deleted");
+      //console.log("todo deleted")
     )
   }
+
+   onChangePhrase(event: Event): void {
+    this.phraseString = (event.target as HTMLInputElement).value;
+  }
+
 }
